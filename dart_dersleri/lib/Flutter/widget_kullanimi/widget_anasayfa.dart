71951898c934e314f4f49730c196ddb3a1cb1542
kerem.kilic <keremkilic_@hotmail.com>
2023-10-26 11:58:43 +0300

    Widget calisma
    widget çalışmaları

--- dart_dersleri/lib/Flutter/widget_kullanimi/widget_anasayfa.dart
+++ dart_dersleri/lib/Flutter/widget_kullanimi/widget_anasayfa.dart
@@ -11,6 +11,13 @@ class _WidgetAnaSayfaState extends State<WidgetAnaSayfa> {
   var tfController = TextEditingController();
   String alinanVeri = "";
   String resimAdi = "mutlu.png";
+  bool switchKontrol = false;
+  bool checkboxKontrol = false;
+  int radioDeger = 0;
+  bool progressKontrol = false;
+  double ilerleme = 30.0;
+  var tfSaat = TextEditingController();
+  var tfTarih = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,54 +30,229 @@ class _WidgetAnaSayfaState extends State<WidgetAnaSayfa> {
         title: const Text("Widgets Kullanımı"),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(ekranYuksekligi / 30),
-              child: Text(alinanVeri),
-            ),
-            Padding(
-              padding: EdgeInsets.all(ekranGenisligi / 40),
-              child: TextField(
-                controller: tfController,
-                decoration: const InputDecoration(
-                  hintText: "Veri",
-                ),
-                keyboardType: TextInputType.number,
-                obscureText: true,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // TextField alanına girilen değeri gösterme
+              Padding(
+                padding: EdgeInsets.all(ekranYuksekligi / 30),
+                child: Text(alinanVeri),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  alinanVeri = tfController.text;
-                });
-              },
-              child: const Text("Veriyi Al"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      resimAdi = "mutlu.png";
-                    });
-                  },
-                  child: const Text("Resim 1"),
+              // TextField alanına girilen degeri tutma
+              Padding(
+                padding: EdgeInsets.all(ekranGenisligi / 40),
+                child: TextField(
+                  controller: tfController,
+                  decoration: const InputDecoration(
+                    hintText: "Veri",
+                  ),
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
                 ),
-                Image.asset("assets/images/$resimAdi"),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      resimAdi = "uzgun.png";
-                    });
-                  },
-                  child: const Text("Resim 2"),
-                ),
-              ],
-            ),
-          ],
+              ),
+              // TextField alanına girilen değeri text aktarma
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    alinanVeri = tfController.text;
+                  });
+                },
+                child: const Text("Veriyi Al"),
+              ),
+              // Butonlara tanımlanan resimleri ekranda gösterme
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        resimAdi = "mutlu.png";
+                      });
+                    },
+                    child: const Text("Resim 1"),
+                  ),
+                  Image.asset("assets/images/$resimAdi"),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        resimAdi = "uzgun.png";
+                      });
+                    },
+                    child: const Text("Resim 2"),
+                  ),
+                ],
+              ),
+              // Switch ve ChechBox kullanımı
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: SwitchListTile(
+                      title: const Text("Dart"),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      value: switchKontrol,
+                      onChanged: (value) {
+                        setState(() {
+                          switchKontrol = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: CheckboxListTile(
+                      title: const Text("Flutter"),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      value: checkboxKontrol,
+                      onChanged: (value) {
+                        setState(() {
+                          checkboxKontrol = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              // Switch ve ChechBox seçimine göre durumlarını gösterme -- true(seçili), false(seçili değil)
+              ElevatedButton(
+                onPressed: () {
+                  print("Switch durum : $switchKontrol");
+                  print("Check durum : $checkboxKontrol");
+                },
+                child: const Text("Göster"),
+              ),
+              // RadioButton kullanımı
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: RadioListTile(
+                      title: const Text("Barcelona"),
+                      value: 1,
+                      groupValue: radioDeger,
+                      onChanged: (value) {
+                        setState(() {
+                          radioDeger = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: RadioListTile(
+                      title: const Text("Real Madrid"),
+                      value: 2,
+                      groupValue: radioDeger,
+                      onChanged: (value) {
+                        setState(() {
+                          radioDeger = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              // RadioButton seçimine göre durumunu gösterme -- true(seçili), false(seçili değil)
+              ElevatedButton(
+                onPressed: () {
+                  print("Switch durum : $switchKontrol");
+                  print("Check durum : $checkboxKontrol");
+                  print("Radio durum : $radioDeger");
+                },
+                child: const Text("Göster"),
+              ),
+              // ProgressBar kullanımı butonlara basarak başla - bitir işlemi yapacak
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        progressKontrol = true;
+                      });
+                    },
+                    child: const Text("Başla"),
+                  ),
+                  Visibility(
+                      visible: progressKontrol,
+                      child: const CircularProgressIndicator()),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        progressKontrol = false;
+                      });
+                    },
+                    child: const Text("Dur"),
+                  )
+                ],
+              ),
+              // Slider
+              Text(ilerleme.toInt().toString()),
+              Slider(
+                max: 100.0,
+                min: 0.0,
+                value: ilerleme,
+                onChanged: (value) {
+                  setState(() {
+                    ilerleme = value;
+                  });
+                },
+              ),
+              // Saat ve Tarih alma işlemi
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 120,
+                    child: TextField(
+                      controller: tfSaat,
+                      decoration: InputDecoration(hintText: "Saat"),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showTimePicker(
+                                context: context,
+                                initialTime:
+                                    TimeOfDay.fromDateTime(DateTime.now()))
+                            .then((value) {
+                          tfSaat.text = "${value!.hour}:${value.minute} ";
+                        });
+                      },
+                      icon: Icon(Icons.access_time)),
+                  SizedBox(
+                    width: 120,
+                    child: TextField(
+                      controller: tfTarih,
+                      decoration: InputDecoration(hintText: "Tarih"),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2030),
+                        ).then((value) {
+                          tfTarih.text = "${value!.day}/${value.month}/${value.year}";
+                        });
+                      },
+                      icon: Icon(Icons.date_range_outlined)),
+                ],
+              ),
+              // Seçilen tarih ve saati gösterme
+              ElevatedButton(
+                onPressed: () {
+                  print("Tarih durum : ${tfTarih.text}");
+                  print("Saat durum : ${tfSaat.text}");
+                },
+                child: const Text("Göster"),
+              ),
+            ],
+          ),
         ),
       ),
     );
